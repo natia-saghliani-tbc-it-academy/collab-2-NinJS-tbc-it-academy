2f285a2f71d276d2818534fe3ba6b52f8eb4b5e9
mikheili-silagava-tbc-it-academy <mikheili.silagava.1@btu.edu.ge>
2025-11-10 21:12:21 +0400

    initial commit

---
+++ JS/misho.js
@@ -0,0 +1,10 @@
+// const palyersAddElement = document.querySelector('.players-section-add') {
+
+// }
+
+function playersAdd() {
+    const playersInputElement = document.querySelector('#players-input')
+    let playerUserName = playersInputElement.textContent
+
+    console.log("playerUserName: ", playerUserName)
+}
